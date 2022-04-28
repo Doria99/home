cb6a2a60ac4c646844187a0379cc05e88a41a4b2
Doria99 <michael.doria.ytct@alice.it>
2022-04-28 14:25:47 +0000

    Updates

---
+++ precache-manifest.3e31ffa86d51769e2408e9ae37086321.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a7589a31cd5ea1a12100a13e3f269b0",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "a5b074d128150f73dd00",
+    "url": "/home/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "a3b1a683b0dcdf6d6db7",
+    "url": "/home/static/js/2.6d125ced.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.6d125ced.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5b074d128150f73dd00",
+    "url": "/home/static/js/main.f2dd3c90.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "025359be24e2453815c5af8deda9a912",
+    "url": "/home/static/media/img.025359be.png"
+  },
+  {
+    "revision": "d0abc4a6713697a766b104b54984ab3c",
+    "url": "/home/static/media/resume.d0abc4a6.pdf"
+  }
+]);
